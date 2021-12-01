ac99184383513d7f5e1f4078ae30c0cbbd90c328
Andrea <keerakobold@gmail.com>
2021-12-01 19:08:52 -0300

    cleaning AddUser. Temporary discarded email validation.

--- src/components/FormAddUser.js
+++ src/components/FormAddUser.js
@@ -14,48 +14,45 @@ const FormAddUser = ({setToggleAddUserForm, getUsers}) => {
   const [emailOK, setEmailOK] = useState(false)
   const [sendForm, setSendForm] = useState(false)
 
-  const simplevalidation = (email) => {
-    var mail_format = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-    if(mail_format.test(email)){
-      setEmailOK(true)
-    } else {
-      setEmailOK(false)
-    }
+  const addUser = async() => {
+    console.log(emailOK)
+    console.log('put')
+    const user = await axios.post('https://618ff3c8f6bf450017484ae0.mockapi.io/api/content/users', newUser)
+    if (user) getUsers();
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSendForm(true)
+    if (emailOK) addUser()
+  }
+
+  const validateData = (data) => {
+    if (!data.name) return false
+    else if (!data.company) return false
+    else if (!data.email) return false
+    else return true
   }
 
   const handleChange = (e) => {
-      const newdata = {[e.target.name]: e.target.value}
-      setNewUser(olduser => ({...olduser, ...newdata}))
-      simplevalidation(newUser.email);
-      console.log('email', newUser.email, emailOK)
-  }
-
-  const handleSend = (e) => {
-    e.preventDefault();
-    setSendForm(true);
-    addUser();
-  }
-
-  const addUser = async() => {
-    if (newUser.company && newUser.name && emailOK) {      
-      let respuesta = await axios.post('https://618ff3c8f6bf450017484ae0.mockapi.io/api/content/users', newUser)
-      if (respuesta) {
-        setNewUser(clean)
-        getUsers()
-    }}
-  }
+    setNewUser({...newUser, [e.target.name]: e.target.value})
+    setEmailOK(validateData(newUser))
+  }  
 
   return (
       <div className='bg-yellow-500 mx-10 my-3 p-3 text-sm'>
-      <form onSubmit={(e) => {handleSend(e)}} className='bg-yellow-200 flex flex-row items-center my-1'>
+
+        <pre>{JSON.stringify(newUser, null, 2)}</pre>
+
+      <form onSubmit={handleSubmit} className='bg-yellow-200 flex flex-row items-center my-1'>
         <div className='flex flex-wrap flex-1'>
           <input className={`p-2 m-1 flex flex-1 focus:bg-white ${!sendForm || newUser.name ? 'bg-yellow-50' : 'bg-red-200'}`}
-            placeholder={newUser.name ? 'Name' : 'Please write username'}
+            placeholder={!sendForm || newUser.name ? 'Name' : 'Please write username'}
             name='name'
             value={newUser.name}
             onChange={handleChange}/>
           <input className={`p-2 m-1 flex flex-1 focus:bg-white ${!sendForm || newUser.company ? 'bg-yellow-50' : 'bg-red-200'}`}
-            placeholder={newUser.company ? 'Company' : 'Please write company name'}
+            placeholder={!sendForm || newUser.company ? 'Company' : 'Please write company name'}
             name='company'
             value={newUser.company}
             onChange={handleChange}/>
@@ -65,7 +62,7 @@ const FormAddUser = ({setToggleAddUserForm, getUsers}) => {
             value={newUser.catchPhrase}
             onChange={handleChange}/>
           <input className={`p-2 m-1 flex flex-1 focus:bg-white ${!sendForm || emailOK ? 'bg-yellow-50' : 'bg-red-200'}`}
-            placeholder={emailOK ? 'E-mail' : 'Please write e-mail'}
+            placeholder={!sendForm || emailOK ? 'E-mail' : 'Please write e-mail'}
             name='email'
             value={newUser.email}
             onChange={handleChange}/>
